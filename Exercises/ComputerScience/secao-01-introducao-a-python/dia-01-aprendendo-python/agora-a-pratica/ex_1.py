873dc2233b64540bbbeba75caab9ba73dc55b386
Fellipe Marcel <fmrclms@gmail.com>
2023-10-20 13:47:30 -0300

    saving changes

---
+++ Exercises/ComputerScience/secao-01-introducao-a-python/dia-01-aprendendo-python/agora-a-pratica/ex_1.py
@@ -0,0 +1,5 @@
+def biggerNumber(num1: float, num2: float) -> float:
+    if num1 > num2:
+        return num1
+    else:
+        return num2
---
+++ Exercises/ComputerScience/secao-01-introducao-a-python/dia-01-aprendendo-python/agora-a-pratica/ex_2.py
@@ -0,0 +1,8 @@
+mediaList = [5, 10, 15, 20, 25, 30]
+
+
+def calculateAverage(media):
+    len(media)
+
+
+print(calculateAverage(mediaList))
